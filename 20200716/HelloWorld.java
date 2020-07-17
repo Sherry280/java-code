e5ce5052c27b33dadb764e2b7257899518a36b6c
Cherry <2827092620@qq.com>
2020-07-17 21:12:10 +0800

    2020/7/17

---
+++ 20200716/HelloWorld.java
@@ -0,0 +1,121 @@
+public class HelloWorld {
+   public static void main(String[] args) {
+       String name="zhangsan";
+       //字符串+操作，表示字符串拼接
+    String a="hello";
+    String b="world";
+    String c=a+b;
+    System.out.println(c);
+//string也可打印多个字符串和数字
+//
+    int d=10;
+    int e=20;
+    System.out.println("d="+d+",e="+e);
+
+   }
+}
+
+
+//        boolean value=true;
+//        System.out.println(value);
+//    }
+// }
+
+   // 	  short s=5;
+   // 	  System.out.println(s);
+   // 	}
+   // }
+
+//         byte a=10;
+//         System.out.println(a);
+//     }
+// }
+
+//       char ch='A';
+//       System.out.println(ch);
+//       char ch2='高';
+//       System.out.println(ch2);
+//       char ch3='9';
+//       System.out.println(ch3);
+//    }
+// }
+
+
+//       float f=12.5f;
+//       System.out.println(f);
+//   }
+// }
+
+
+
+
+//      double d=12.5;
+//      System.out.println(d);
+//      double num=1.1;
+//      System.out.println(num*num);
+//  }
+// }
+
+//        long a=10;
+//        System.out.println(a);
+//        System.out.println(Long.MAX_VALUE);
+//        System.out.println(Long.MIN_VALUE);
+//    }
+// }
+
+
+// 	int a=10;
+// 	System.out.println(a);
+// 	System.out.printf("%d\n",a);//格式化输出
+// 	System.out.print(a);//不会换行
+// 	System.out.println("baozi");
+//    }
+// }
+
+//     public static void main(String[] args) {
+//     	int a=10;
+//     	int b=20;
+//     	int c=30;
+//     	int temp=0;
+//     	if(a>b){
+//     		temp=a;
+//     		a=b;
+//     		b=temp;	
+//     	}
+//     	if(a>c){
+//     		temp=a;
+//     		a=c;
+//     		c=temp;
+//     	}
+//     	if(b>c){
+//     		temp=b;
+//     		b=c;
+//     		c=temp;
+
+//     	}
+//     	System.out.println("maxValue="+c+"\t"+"minValue="+a);
+//     }
+// }
+
+
+// //给定两个int变量的值，交换变量的值
+//     public static void main(String[] args) {
+// 	    int a=10;
+// 	    int b=5;
+// 	    int num=0;
+// 	    num=a;
+// 	    a=b;
+// 	    b=num;
+// 	    System.out.println(a);
+// 	    System.out.println(b);
+//     }
+
+
+
+
+
+
+
+
+
+
