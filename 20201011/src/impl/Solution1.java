4747f07c75e340ae1333cd67f0c8b8e9e16242f6
Cherry <2827092620@qq.com>
2020-10-12 23:34:47 +0800

    括号匹配

---
+++ 20201011/src/impl/Solution1.java
@@ -0,0 +1,63 @@
+package impl;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class Solution1 {
+    public boolean isVaild(String s){
+        Deque<Character>  stack=new LinkedList<>();
+
+
+        char[] charArray=s.toCharArray();
+        for(char c:charArray) {
+            switch (c) {
+                case '(':
+                case '[':
+                case '{':
+                    stack.push(c);
+                    break;
+                default: {
+                    if (stack.isEmpty()) {
+                        return false;
+                    }
+                    char left = stack.pop();
+                    if (!compareBracket(left, c)) {
+                        return false;
+                    }
+                }
+            }
+        }
+        if(stack.isEmpty()){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+
+    private boolean compareBracket(char left,char right){
+        if(left=='['&&right==']'){
+            return true;
+        }
+        if(left=='('&&right==')'){
+            return true;
+        }
+        if(left=='{'&&right=='}'){
+            return true;
+        }
+        return false;
+    }
+
+
+    public static void main(String[] args) {
+        Solution1 s=new Solution1();
+        String sc="{()}[]";
+        System.out.println(s.isVaild(sc));
+    }
+
+
+
+
+
+
+}
