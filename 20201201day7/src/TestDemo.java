bc2847e9c18ec6e7a2d7d90158a67e3201c926ee
Cherry <2827092620@qq.com>
2020-12-02 23:59:23 +0800

    堆

--- 20201201day7/src/TestDemo.java
+++ 20201201day7/src/TestDemo.java
@@ -1,6 +1,46 @@
 import java.util.Scanner;
 
 public class TestDemo {
+    //盒子里可以放几块蛋糕
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int w=sc.nextInt();
+        int h=sc.nextInt();
+
+        int sum=0;
+        int x1=0;
+        int x2=0;
+        int y1=0;
+        int y2=0;
+
+        for(int i=0;i<w;i++){
+
+        }
+
+
+//        for(int i=0;i<w;i++){
+//            for(int j=i+1;j<h;j++){
+//                while(i!=j){
+//                    int c=(x1-x2)*(x1-x2)+(y1-y2)*(y1-y2);
+//                    if(Math.sqrt(c)!=2){
+//                        sum++;
+//                    }
+//                }
+//
+//                }
+//
+//        }
+
+        System.out.println(sum);
+    }
+
+
+
+
+
+
+
+
     //求最大公倍数
     private static int maxGongYueshu2(int x,int y){
         //辗转相除法
@@ -41,7 +81,7 @@ public class TestDemo {
 
 
     }
-    public static void main(String[] args) {
+    public static void main1(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n1=sc.nextInt();
         int n2=sc.nextInt();
