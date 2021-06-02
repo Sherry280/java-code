e3d186270ed977d1c6eb14d8ea58e846d2b5175c
Cherry <2827092620@qq.com>
2021-06-02 22:52:19 +0800

    每日一题

---
+++ LeetCode06/src/Day0602/Solution1.java
@@ -0,0 +1,40 @@
+package Day0602;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution1 {
+    /**
+     * 给你一个整数数组 nums 和一个整数 k ，编写一个函数来判断该数组是否含有同时满足下述条件的连续子数组：
+     * 子数组大小 至少为 2 ，且子数组元素总和为 k 的倍数。
+     * 如果存在，返回 true ；否则，返回 false 。
+     * 如果存在一个整数 n ，令整数 x 符合 x = n * k ，则称 x 是 k 的一个倍数。
+     */
+    public static  boolean checkSubarraySum(int[] nums, int k) {
+        int m = nums.length;
+        if (m < 2) {
+            return false;
+        }
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        map.put(0, -1);
+        int remainder = 0;
+        for (int i = 0; i < m; i++) {
+            remainder = (remainder + nums[i]) % k;
+            if (map.containsKey(remainder)) {
+                int prevIndex = map.get(remainder);
+                if (i - prevIndex >= 2) {
+                    return true;
+                }
+            } else {
+                map.put(remainder, i);
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        int[] nums={2,3,4,5,6};
+        int k=4;
+        System.out.println(checkSubarraySum(nums, k));
+
+    }
+}
