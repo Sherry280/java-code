f695182486c5fce0e50478cb5c8f37d30feb3cf6
Cherry <2827092620@qq.com>
2021-06-27 10:08:13 +0800

    每日一题

---
+++ LeetCode06/src/Day0626/Solution2.java
@@ -0,0 +1,60 @@
+package Day0626;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Solution2 {
+    /**
+     *广度优先遍历：蛇梯棋
+     * -1代表当前棋盘位置为空，正整数代表当前可以进行跳转到的位置
+     * 使用一个队列，存储
+     * 分为奇数行和偶数行来进行处理
+     */
+    public int snakesAndLadders(int[][] board) {
+        int n=board.length;//行数
+        boolean[] vis=new boolean[n*n+1];
+        Queue<int[]> queue=new LinkedList<>();
+        queue.offer(new int[]{1,0});//初始状态
+        while(!queue.isEmpty()){
+            int[] p=queue.poll();
+            for(int i=1;i<=6;i++){
+                int nxt=p[0]+i;
+                if(nxt>n*n){
+                    break;
+                }
+                int[] rc = id2rc(nxt, n); // 得到下一步的行列
+                if (board[rc[0]][rc[1]] > 0) { // 存在蛇或梯子
+                    nxt = board[rc[0]][rc[1]];
+                }
+                if (nxt == n * n) { // 到达终点
+                    return p[1] + 1;
+                }
+                if (!vis[nxt]) {
+                    vis[nxt] = true;
+                    queue.offer(new int[]{nxt, p[1] + 1}); // 扩展新状态
+                }
+            }
+        }
+        return -1;
+    }
+
+    public int[] id2rc(int id, int n) {
+        int r = (id - 1) / n, c = (id - 1) % n;
+        if (r % 2 == 1) {
+            c = n - 1 - c;
+        }
+        return new int[]{n - 1 - r, c};
+    }
+
+    public static void main(String[] args) {
+        int[][] board={{-1,-1,-1,-1,-1,-1},
+                {-1,-1,-1,-1,-1,-1},
+                {-1,-1,-1,-1,-1,-1},
+                {-1,35,-1,-1,13,-1},
+                {-1,-1,-1,-1,-1,-1},
+                {-1,15,-1,-1,-1,-1}};
+        Solution2 s=new Solution2();
+        System.out.println(s.snakesAndLadders(board));
+
+    }
+}
