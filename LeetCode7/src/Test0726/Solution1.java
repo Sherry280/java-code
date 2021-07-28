e8863be8963d664819f6603a578e61dd707459f6
Cherry <2827092620@qq.com>
2021-07-28 10:08:46 +0800

    Test

---
+++ LeetCode7/src/Test0726/Solution1.java
@@ -0,0 +1,23 @@
+package Test0726;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Solution1 {
+    /**
+     *
+     */
+    public int[] restoreArray(int[][] adjacentPairs){
+        int[] re=new int[adjacentPairs.length+1];
+        return re;
+    }
+
+    public static void main(String[] args) {
+        int[][] array={{1,2},{2,3},{3,4}};
+        int[] a=array[0];
+        int[] b=array[1];
+        System.out.println(Arrays.toString(a));
+
+
+    }
+}
