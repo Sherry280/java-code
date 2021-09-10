648d9844e4566903200e04d84e7df85fe6ebabb4
Sherry280 <62702083+Sherry280@users.noreply.github.com>
2021-09-10 23:49:37 +0800

    Add files via upload

--- Solution1.java
+++ Solution1.java
@@ -1,80 +1,87 @@
-package Test09.Test0904;
-
-import com.sun.jmx.snmp.SnmpUnknownMsgProcModelException;
+package Test09.Test0910;
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Solution1 {
-    /**
-     * 构建乘积数组：新数组为当前数组中除过第i项的所有数字的乘积
-     * @param A 不能使用除法，那么就是不能将A中所有元素相乘，再除以A[i]
-     *          使用两边相乘的方法；
-     * @return
-     */
-    public int[] multiply(int[] A) {
-        if(A.length==0){
-            return null;
-        }
-        int[] res=new int[A.length];
-        for(int i=0;i<A.length;i++){
-            //i=3
-            //计算i左边的乘积0-i-1
-            int sumLeft=1;
-            int sumRight=1;
-            int left=0;
-            int right=i+1;
-            //l从0开始计算，
-            while(left<i){
-               sumLeft*=A[left];
-               left++;
-            }
-            //计算i右边的乘积i+1-A.length-1;
-            while(right<A.length){
-                sumRight*=A[right];
-                right++;
-            }
-            res[i]=sumLeft*sumRight;
 
-        }
-        return res;
 
-    }
-    public String replaceSpace (String s) {
-        char[] chars = s.toCharArray();
-        String res=s;
-        for(int i=0;i<chars.length;i++){
-            if(chars[i]==' '){
-                res+="%20";
-            }else{
-                res+=chars[i];
-            }
-        }
-        return res;
-    }
     /**
-     *剑指offer：扑克牌排序是否为顺子：输入数据时自行进行转换，将A转为1，将J转为11
+     * 数组中最长连续子序列
+     * [1,2,3,4,100,200]
      */
-    public static boolean IsContinuous(int [] numbers) {
-        //如果没有重复，并且最后一个元素减去当前第一个非0的数字的差<=4
-        //那么就说明此数组元素有序
-        int res=0;
-        int zero=0;
-        Arrays.sort(numbers);
-        for(int i=0;i<numbers.length;i++){
-            if(numbers[i]==0){
-                zero++;
+    public int MaxLength(int[] arr){
+        //不进行排序，使用set进行存储
+        if(arr.length==0||arr==null){
+            return 0;
+        }
+        Set<Integer> set=new HashSet<>();
+        for(int e:arr){
+            set.add(e);
+        }
+        int longest=0;
+        //找到set中元素的最小的一个
+        for(int a:arr){
+            if(set.contains(a-1)){
                 continue;
             }
-            if(i!=0&&numbers[i]== numbers[i-1]){
-                return false;
+            int currNum=a;
+            int count=0;
+            while(set.contains(a+1)){
+                currNum++;
+                count++;
             }
+            longest=Math.max(longest,count);
+
+
         }
-        return numbers[numbers.length-1]-numbers[zero]<=4;
+        return longest;
+    }
+    public int MLS(int[] arr) {
+        if(arr.length==0||arr==null){
+            return 0;
+        }
+        Arrays.sort(arr);
+        int count=arr[0];
+        int longest=1;
+
+        for(int i=1;i<arr.length;i++){
+            //遇到重复的元素,直接跳过
+            if(arr[i]==arr[i-1]){
+                continue;
+            }
+           if(arr[i]-arr[i-1]==1){
+               count++;
+           }else{
+               //遇到数字和当前值相等，或者遇到的数字比当前的数字小
+               count=1;
+           }
+           longest=Math.max(longest,count);
+
+        }
+        return longest;
     }
 
-    public static void main(String[] args) {
-        int[] numbers={1,2,3,0,0};
+    /**
+     *[10,20,30,40,5,60]
+     * 最大升序子数组的和
+     */
+    public int maxAscendingSum(int[] nums) {
+        int max=nums[0];
+        int sum=nums[0];
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]>nums[i-1]){
+                sum+=nums[i];
 
-        System.out.println(IsContinuous(numbers));
+            }else{
+                sum=nums[i];
+            }
+            max=Math.max(max,sum);
+        }
+        return max;
+    }
+    public static void main(String[] args) {
+
     }
 }
