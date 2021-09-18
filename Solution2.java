55b246396f1a3a0b245413320ae9cb2078638cb7
Sherry280 <62702083+Sherry280@users.noreply.github.com>
2021-09-18 22:40:49 +0800

    Add files via upload
    练习

--- Solution2.java
+++ Solution2.java
@@ -1,83 +1,56 @@
-package Test09.Test0913;
+package Test09.Test0916;
 
-import java.util.HashSet;
-import java.util.Scanner;
-import java.util.Set;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 public class Solution2 {
-
-    public int[] max_sub_matrix (int[][] matrix) {
-        //先求出最大的子矩阵,再求出子矩阵的和
-        int m=matrix.length;
-        int n=matrix[0].length;
-        int[][] res=new int[m][n];
-        for(int i=0;i<m;i++){
-            res[i][0]=matrix[i][0];
-
-        }
-        for(int j=0;j<n;j++){
-            res[0][j]=matrix[0][j];
-        }
-        for(int i=1;i<m;i++){
-            for(int j=1;j<n;j++){
-                if(matrix[i][j]==1){
-                    res[i][j]=Math.min(res[i][j-1],Math.min(res[i-1][j-1],res[i-1][j]))+1;
-                }else{
-                    res[i][j]=0;
-                }
+    public static String removeWords(String s){
+        Map<Character,Integer> map=new HashMap<>();
+        char[] chars=s.toCharArray();
+        for(int i=0;i<chars.length;i++){
+            if(!map.containsKey(chars[i])){
+                map.put(chars[i],1);
+            }else{
+                map.put(chars[i],map.get(chars[i])+1);
             }
         }
-        int max=res[0][0];
-        int max_i=0;
-        int max_j=0;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(res[i][j]>max){
-                    max=res[i][j];
-                    max_i=i;
-                    max_j=j;
-                }
+        Iterator<Map.Entry<Character,Integer>> it=map.entrySet().iterator();
+        while(it.hasNext()){
+            Map.Entry<Character,Integer> entry=it.next();
+            char key=entry.getKey();
+            int value=entry.getValue();
+            if(value>=3){
+                it.remove();
             }
         }
-        int sum=0;
-        for(int i=max_i-max+1;i<max_i;i++){
-            for(int j=max_j-max+1;j<max_i;j++){
-                sum+=matrix[i][j];
+//        for(Map.Entry<Character,Integer> e:map.entrySet()){
+//            char key=e.getKey();
+//            int value=e.getValue();
+//            if(value>=3){
+//                map.remove(key);
+//            }
+//        }
+        //将map中剩余的字符拿出来 b:2 c:1   111222gggbcb
+        String res="";
+        for(int i=0;i<chars.length;i++){
+            if(map.containsKey(chars[i])){
+                res+=chars[i];
             }
         }
-        int[] ret=new int[5];
-        for(int i=0;i<5;i++){
-            ret[0]=sum;
-
-        }
-        return  ret;
+//        for(Map.Entry<Character,Integer> e:map.entrySet()){
+//            char key=e.getKey();
+//            int value=e.getValue();
+//            if(k<s.length()&&chars[k]==key){
+//                res+=chars[k];
+//                k=k+1;
+//            }
+//        }
+        return  res;
     }
-    public static int findMediaArrays(int[] arr1,int[] arr2){
-        return 0;
-    }
-    /**
-     *"DKAFWADCBEILBCEEA","BCA"
-     * 找到字符串b在a中的最小的子串
-     *
-     */
-    public static String minSplitWords(String a,String b){
-        Set<String> set=new HashSet<>();
-        for(int i=0;i<=a.length()-b.length();i++){
-            char a1=a.charAt(i);
-            char b1=b.charAt(i);
-            for(int j=0;j<b.length();j++){
 
-            }
-        }
-        return null;
-    }
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        while(sc.hasNext()){
-            String A=sc.nextLine();
-            String B=sc.nextLine();
-            System.out.println(minSplitWords(A, B));
-        }
-
+        String s="1177j";
+        System.out.println(removeWords(s));
     }
 }
