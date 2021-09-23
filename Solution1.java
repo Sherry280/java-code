4e7b874b8cd4630f6a09ab226cd8ebe9605be435
Sherry280 <62702083+Sherry280@users.noreply.github.com>
2021-09-23 21:15:08 +0800

    Add files via upload
    练习

--- Solution1.java
+++ Solution1.java
@@ -1,87 +1,33 @@
-package Test09.Test0910;
-
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+package Test09.Test0923;
 
 public class Solution1 {
-
-
     /**
-     * 数组中最长连续子序列
-     * [1,2,3,4,100,200]
+     *计算字符串中不含有重复字符的最长字符串
      */
-    public int MaxLength(int[] arr){
-        //不进行排序，使用set进行存储
-        if(arr.length==0||arr==null){
-            return 0;
-        }
-        Set<Integer> set=new HashSet<>();
-        for(int e:arr){
-            set.add(e);
-        }
-        int longest=0;
-        //找到set中元素的最小的一个
-        for(int a:arr){
-            if(set.contains(a-1)){
-                continue;
+    public static int findNoRepeatSubStringLength (String str) {
+        int start=0;
+        int end=0;
+        char[] chars = str.toCharArray();
+        int length=0;
+        int max=0;
+        while(end<chars.length){
+            char c=chars[end];
+            for(int i=start;i<end;i++){
+                if(c==chars[i]){
+                    start=i+1;
+                    length=end-start;
+                    break;
+                }
             }
-            int currNum=a;
-            int count=0;
-            while(set.contains(a+1)){
-                currNum++;
-                count++;
-            }
-            longest=Math.max(longest,count);
-
-
-        }
-        return longest;
-    }
-    public int MLS(int[] arr) {
-        if(arr.length==0||arr==null){
-            return 0;
-        }
-        Arrays.sort(arr);
-        int count=arr[0];
-        int longest=1;
-
-        for(int i=1;i<arr.length;i++){
-            //遇到重复的元素,直接跳过
-            if(arr[i]==arr[i-1]){
-                continue;
-            }
-           if(arr[i]-arr[i-1]==1){
-               count++;
-           }else{
-               //遇到数字和当前值相等，或者遇到的数字比当前的数字小
-               count=1;
-           }
-           longest=Math.max(longest,count);
-
-        }
-        return longest;
-    }
-
-    /**
-     *[10,20,30,40,5,60]
-     * 最大升序子数组的和
-     */
-    public int maxAscendingSum(int[] nums) {
-        int max=nums[0];
-        int sum=nums[0];
-        for(int i=1;i<nums.length;i++){
-            if(nums[i]>nums[i-1]){
-                sum+=nums[i];
-
-            }else{
-                sum=nums[i];
-            }
-            max=Math.max(max,sum);
+            length++;
+            end++;
+            max=max<length?length:max;
         }
         return max;
     }
     public static void main(String[] args) {
+        String s="abcabcadbb";
+        System.out.println(findNoRepeatSubStringLength(s));
 
     }
 }
