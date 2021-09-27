c7a49381f1b056f914896bffff4aa459765a77fb
Sherry280 <62702083+Sherry280@users.noreply.github.com>
2021-09-27 23:56:05 +0800

    Add files via upload
    练习

--- Solution2.java
+++ Solution2.java
@@ -1,56 +1,24 @@
-package Test09.Test0916;
+package Test09.Test0923;
 
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
 
 public class Solution2 {
-    public static String removeWords(String s){
-        Map<Character,Integer> map=new HashMap<>();
-        char[] chars=s.toCharArray();
-        for(int i=0;i<chars.length;i++){
-            if(!map.containsKey(chars[i])){
-                map.put(chars[i],1);
-            }else{
-                map.put(chars[i],map.get(chars[i])+1);
+    /**
+     * 1，6，[9,3]  0   7
+     * 判断数组中的值能否经过k次变成target
+     */
+    public int qualifiedQuantity (int k, int target, int[] number) {
+        int count=0;
+        for(int i=0;i<number.length;i++){
+            if((number[i]>=target)&&(Math.abs(number[i]-k)<=target)){
+                count++;
+            }
+            if(number[i]+k>=target){
+                count++;
             }
         }
-        Iterator<Map.Entry<Character,Integer>> it=map.entrySet().iterator();
-        while(it.hasNext()){
-            Map.Entry<Character,Integer> entry=it.next();
-            char key=entry.getKey();
-            int value=entry.getValue();
-            if(value>=3){
-                it.remove();
-            }
-        }
-//        for(Map.Entry<Character,Integer> e:map.entrySet()){
-//            char key=e.getKey();
-//            int value=e.getValue();
-//            if(value>=3){
-//                map.remove(key);
-//            }
-//        }
-        //将map中剩余的字符拿出来 b:2 c:1   111222gggbcb
-        String res="";
-        for(int i=0;i<chars.length;i++){
-            if(map.containsKey(chars[i])){
-                res+=chars[i];
-            }
-        }
-//        for(Map.Entry<Character,Integer> e:map.entrySet()){
-//            char key=e.getKey();
-//            int value=e.getValue();
-//            if(k<s.length()&&chars[k]==key){
-//                res+=chars[k];
-//                k=k+1;
-//            }
-//        }
-        return  res;
-    }
+        return count;
 
-    public static void main(String[] args) {
-        String s="1177j";
-        System.out.println(removeWords(s));
     }
 }
