12f19c190b1d273e16d9b3276759e31337a5724e
Sherry280 <62702083+Sherry280@users.noreply.github.com>
2021-09-29 22:13:32 +0800

    Add files via upload

--- Solution2.java
+++ Solution2.java
@@ -1,24 +1,62 @@
-package Test09.Test0923;
+package Test09.Test0927;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Arrays;
 
 public class Solution2 {
     /**
-     * 1，6，[9,3]  0   7
-     * 判断数组中的值能否经过k次变成target
+     * 返回拼接后的最短的回文串
      */
-    public int qualifiedQuantity (int k, int target, int[] number) {
-        int count=0;
-        for(int i=0;i<number.length;i++){
-            if((number[i]>=target)&&(Math.abs(number[i]-k)<=target)){
-                count++;
+    public static String shortestPalindrome (String s) {
+        int n=s.length();
+        int[] ret=new int[n];
+        Arrays.fill(ret,-1);
+        for(int i=1;i<n;i++){
+            int j=ret[i-1];
+            while(j!=-1&&s.charAt(j+1)!=s.charAt(i)){
+                j=ret[i];
             }
-            if(number[i]+k>=target){
-                count++;
+            if(s.charAt(j+1)==s.charAt(i)){
+                ret[i]=j+1;
             }
         }
-        return count;
+       int best=-1;
+        for(int i=n-1;i>=0;i--){
+            while(best!=-1&&s.charAt(best+1)!=s.charAt(i)){
+                best=ret[best];
+            }
+            if(s.charAt(best+1)==s.charAt(i)){
+                ++best;
+            }
+        }
+        String add=(best==n-1?"":s.substring(best+1));
+        StringBuffer sb=new StringBuffer(add).reverse();
+        sb.append(s);
+        return  sb.toString();
+    }
+    /**
+     * 找出重复的那个数字 1,2,4,4
+     */
+    public static int findDuplicate (int[] nums) {
+        int i=0;
+        int temp=0;
+        while(true){
+            if(nums[i]==0){
+                return temp;
+            }
+            temp=nums[i];
+            nums[i]=0;
+            i=temp;
+        }
+//        int[] ret=new int[n];
+//        for(int i=0;i<n;i++){
+//            ret[i-'0']++;
+//        }
+
+
+    }
+    public static void main(String[] args) {
+        String s="aaeceaaa";
+        System.out.println(shortestPalindrome(s));
 
     }
 }
