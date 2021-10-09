327dbf170618260b89bdf6c4c5db455d5610645c
Sherry280 <62702083+Sherry280@users.noreply.github.com>
2021-10-09 20:36:42 +0800

    Add files via upload
    test

--- Solution1.java
+++ Solution1.java
@@ -1,33 +1,76 @@
-package Test09.Test0923;
+package Test10.Test1009;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
 public class Solution1 {
     /**
-     *计算字符串中不含有重复字符的最长字符串
+     * 1,2,3,2,2,3
+     * 找出一个数组中最多的数字所在的最小长度
+     * 输出2，5
+     *
      */
-    public static int findNoRepeatSubStringLength (String str) {
-        int start=0;
-        int end=0;
-        char[] chars = str.toCharArray();
-        int length=0;
-        int max=0;
-        while(end<chars.length){
-            char c=chars[end];
-            for(int i=start;i<end;i++){
-                if(c==chars[i]){
-                    start=i+1;
-                    length=end-start;
-                    break;
-                }
+    public static void maxLength(int n,int[] array){
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<array.length;i++){
+            if(!map.containsKey(array[i])){
+                map.put(array[i],1);
+            }else{
+                map.put(array[i],map.get(array[i])+1);
             }
-            length++;
-            end++;
-            max=max<length?length:max;
         }
-        return max;
+        int max=0;
+        int num=0;
+        for(Map.Entry<Integer,Integer> e:map.entrySet()){
+            int key=e.getKey();
+            int value=e.getValue();
+            if(value>=max) {
+                max = Math.max(max, value);
+                num=key;
+            }
+        }
+        //现在找到出现次数最多的那个数字
+        int start=0;
+
+        for(int i=0;i<array.length;i++){
+            if(array[i]==num){
+                start=i+1;
+                break;
+            }
+        }
+        int z=start;
+        int count=0;
+        int end=start;
+       while(count<num){
+            end++;
+            if(array[start]==num){
+                count++;
+            }
+            start++;
+        }
+        System.out.println(z+" "+end);
+//        Arrays.sort(array);
+//        int count=0;
+//        int ret=array[0];
+//        for(int i=1;i<array.length;i++){
+//            if(ret==array[i]){
+//                count++;
+//            }
+//        }
+
     }
     public static void main(String[] args) {
-        String s="abcabcadbb";
-        System.out.println(findNoRepeatSubStringLength(s));
+        Scanner sc=new Scanner(System.in);
+        while(sc.hasNext()){
+            int n=sc.nextInt();
+            int[] array=new int[n];
+            for(int i=0;i<array.length;i++){
+                array[i]=sc.nextInt();
+            }
+            maxLength(n,array);
+        }
 
     }
 }
